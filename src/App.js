9c07518dd19ccb705a57cf97b79a537cfd00baee
Harshil Gajjar <harshilgajjar54@gmail.com>
2022-12-06 07:54:31 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import React from 'react';
-import './App.css';
-import Dashboard from './components/dashboard/dashboard';
+import React from "react";
+import "./App.css";
+import Dashboard from "./components/dashboard/dashboard";
 
-
-export default class App extends React.Component{
-
-  render(){
-    return (<Dashboard/>)
+export default class App extends React.Component {
+  render() {
+    return <Dashboard />;
   }
 }
-
